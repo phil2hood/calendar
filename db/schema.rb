2687a7a117dadcf824b26ff3ad63c7dd565f2d6a
ssb <phil_hood@phareware.com>
2015-07-10 17:21:29 -0400

    a little clean up

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150706193507) do
+ActiveRecord::Schema.define(version: 20150710182857) do
 
   create_table "calendar_accesses", force: :cascade do |t|
     t.integer  "user_calendar_id", limit: 4
