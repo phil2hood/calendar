d8df8a04847f0dc294e43194e3d99fd23e721870
ssb <phil_hood@phareware.com>
2015-07-14 07:54:30 -0400

    fix settings

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,9 @@
 class RegistrationsController < Devise::RegistrationsController
-  before_filter :configure_permitted_parameters, :only => [:create]
+  before_filter :configure_permitted_parameters
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :timezone, :snap_to_minutes, :default_calendar_id) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :timezone, :snap_to_minutes, :default_calendar_id, :password_confirmation, :current_password) }
   end
 end
